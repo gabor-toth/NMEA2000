a4962973aa46ac2653851b705a4f547ba2f9efc3
Gabor Toth <gabor.toth@p92.hu>
2023-04-17 13:59:33 +0200

    ESP IDF: configure debug level

---
+++ env_support/cmake/esp.h
@@ -0,0 +1,26 @@
+#ifndef _NMEA2000_ENV_ESP_H
+#define _NMEA2000_ENV_ESP_H
+
+#include "sdkconfig.h"
+#include "NMEA2000_esp32_stream.h"
+
+#if defined(CONFIG_NMEA2000_FRAME_ERROR_DEBUG)
+#define NMEA2000_FRAME_ERROR_DEBUG
+#endif
+#if defined(CONFIG_NMEA2000_FRAME_IN_DEBUG)
+#define NMEA2000_FRAME_IN_DEBUG
+#endif
+#if defined(CONFIG_NMEA2000_FRAME_OUT_DEBUG)
+#define NMEA2000_FRAME_OUT_DEBUG
+#endif
+#if defined(CONFIG_NMEA2000_DEBUG)
+#define NMEA2000_DEBUG
+#endif
+#if defined(CONFIG_NMEA2000_MSG_DEBUG)
+#define NMEA2000_MSG_DEBUG
+#endif
+#if defined(CONFIG_NMEA2000_BUF_DEBUG)
+#define NMEA2000_BUF_DEBUG
+#endif
+
+#endif //_NMEA2000_ENV_ESP_H
